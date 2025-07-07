af7f714b725d3a01b4255df1006b4fb39ba8155b
Mohammed-Am <mh.amouzoun@gmail.com>
2025-07-07 22:21:48 +0100

    first commit

--- index.js
+++ index.js
@@ -11,11 +11,7 @@ const port = process.env.PORT || 5000;
 // Configure CORS to allow credentials and specific origin
 //vercel vercel
 app.use(cors({
-  origin: [
-    'https://frontend-afcon-worldcup-morocco2025.vercel.app',
-    'https://frontend-afcon-worldcup-morocc-git-6122d4-mohammed-ams-projects.vercel.app',
-    'https://frontend-afcon-worldcup-morocco2025-organization-qbp37woaz.vercel.app'
-  ],
+  origin: true,
   credentials: true,
 }));
 
